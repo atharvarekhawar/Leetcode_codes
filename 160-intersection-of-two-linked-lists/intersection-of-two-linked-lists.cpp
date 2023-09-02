834ef32aafaec3f945f39db68329781b3af618ac
Atharva Rekhawar <113119063+atharvarekhawar@users.noreply.github.com>
2023-09-02 12:04:03 +0530

    Time: 36 ms (82.19%) | Memory: 14.5 MB (59.97%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,41 +8,51 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,bool>m;
+    int length(ListNode *temp1, ListNode *temp2){
+        int len1=0;
+        int len2=0;
 
-        ListNode* ans=NULL;
+        while(temp1!=NULL || temp2!=NULL){
+            if(temp1!=NULL){
+                len1++;
+                temp1=temp1->next;
+            }
+            if(temp2!=NULL){
+                len2++;
+                temp2=temp2->next;
+            }
+        }
+
+        return len1-len2;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int diff = length(headA,headB);
         ListNode* temp1=headA;
         ListNode* temp2=headB;
 
+        if(diff<0){
+            diff=abs(diff);
+            while(diff>0){
+                temp2=temp2->next;
+                diff--;
+            }
+        }
+        else{
+            diff=abs(diff);
+            while(diff>0){
+                temp1=temp1->next;
+                diff--;
+            }
+        }
+
+        ListNode* ans=NULL;
 
         while(temp1!=NULL && temp2!=NULL){
-            if(m[temp1]==true){
-                ans=temp1;
-                break;
-            }
-            m[temp1]=true;
-            if(m[temp2]==true){
-                ans=temp2;
-                break;
-            }
-            m[temp2]=true;
-            temp1=temp1->next;
-            temp2=temp2->next;
-        }
-
-        while(temp1!=NULL){
-            if(m[temp1]==true){
+            if(temp1==temp2){
                 ans=temp1;
                 break;
             }
             temp1=temp1->next;
-        }
-         while(temp2!=NULL){
-            if(m[temp2]==true){
-                ans=temp2;
-                break;
-            }
             temp2=temp2->next;
         }
 
