aacbf20f7cfee61ec6f912b3f7b26e4c5821286a
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-12-31 07:57:50 +0530

    Time: 3 ms (47.38%) | Memory: 6.6 MB (75.31%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        int n=s.length();
-        if(n<=2){
+        int n = s.length();
+        if (n <= 2) {
             return 0;
         }
 
-        int count=-1;
+        int count = -1;
 
-        for(int i=0;i<n-3;i++){
+        for (int i = 0; i < n - 3; i++) {
             char currChar = s[i];
-            // return i;
-            for(int j=i+1;j<n;j++){
-                //return j;
-                if(s[j]==currChar){
-                    count = max(count,j-i-1);
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (s[j] == currChar) {
+                    count = max(count, j - i - 1);
                 }
             }
         }
